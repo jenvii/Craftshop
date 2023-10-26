ab86c3e58afb04616e99326bd8931225614eabba
Jenna <143401323+jenvii@user.noreply.github.com>
2023-10-26 10:36:38 +0300

    Myytävien tuotteiden poisto lisätty

--- craftshop/src/main/java/sof03/craftshop/web/HandicraftController.java
+++ craftshop/src/main/java/sof03/craftshop/web/HandicraftController.java
@@ -3,6 +3,7 @@ package sof03.craftshop.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -31,6 +32,12 @@ public class HandicraftController {
 	public String saveHandicraft(Handicraft handicraft) {
 		handicraftRepository.save(handicraft);
 		return "redirect:shop";
+	}
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteHandicraft(@PathVariable("id") Long handicraftId, Model model) {
+		handicraftRepository.deleteById(handicraftId);
+		return "redirect:/shop";
 	}
 
 }
